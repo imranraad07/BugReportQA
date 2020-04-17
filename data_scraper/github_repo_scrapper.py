ef3450e501ca7febd66b145d31f8dcb2f4d13fe9
imranm3 <imranm3@egr-u-cs-kd-02.rams.adp.vcu.edu>
2020-04-17 13:41:50 -0400

    project files reorganized and dataset zip added

---
+++ data_scraper/github_repo_scrapper.py
@@ -0,0 +1,72 @@
+import json
+import time
+
+import requests
+
+
+def get_repos(auth, file_name):
+    step = 2500
+    item_count = 0
+    while step > 0:
+        # url = "https://api.github.com/search/repositories?q=stars:{star}+language:java+fork:true+created:>=2017-01-01"
+        # url = "https://api.github.com/search/repositories?q=stars:{star}+language:java+fork:true+created:2014-01-01..2016-12-31"
+        # url = "https://api.github.com/search/repositories?q=stars:{star}+language:java+fork:true+created:<2014-01-01"
+
+        url = ''
+        # data for github star 2500 to infinity
+        if step >= 2500:
+            url = "https://api.github.com/search/repositories?q=stars:>=2500+language:java+fork:true"
+            step = 2200
+        # data for github star 2200 to 2499
+        elif step >= 2200:
+            url = "https://api.github.com/search/repositories?q=stars:>=2200..2499+language:java+fork:true"
+            step = 2000
+        # data for github star 2000 to 2199
+        elif step >= 2000:
+            url = "https://api.github.com/search/repositories?q=stars:>=2000..2199+language:java+fork:true"
+        else:
+            url = "https://api.github.com/search/repositories?q=stars:{star}+language:java+fork:true"
+            url = url.format(star=step)
+            step = step - 1
+
+        link = dict(next=url)
+        # print(link, time.ctime())
+        while 'next' in link:
+            response = requests.get(link['next'], auth=auth)
+            # print(link, response.status_code)
+
+            while response.status_code != 200:
+                # print("Bad response code:", response.status_code, "sleeping for 30 second....", time.ctime())
+                time.sleep(30)
+                response = requests.get(link['next'], auth=auth)
+
+            # print(response.json())
+            for item in response.json()['items']:
+                print(item_count, item['full_name'], item['stargazers_count'], item['has_issues'])
+                if item['has_issues'] is True:
+                    item_count = item_count + 1
+                    with open(file_name, "a") as myfile:
+                        myfile.write(item['full_name'] + "\n")
+
+            link = _link_field_to_dict(response.headers.get('link', None))
+
+
+def _link_field_to_dict(field):
+    if not field:
+        return dict()
+
+    return dict([
+        (
+            part.split('; ')[1][5:-1],
+            part.split('; ')[0][1:-1],
+        ) for part in field.split(', ')
+    ])
+
+
+if __name__ == '__main__':
+    with open('../credentials.json') as json_file:
+        data = json.load(json_file)
+    username = data['username']
+    password = data['password']
+    auth1 = (username, password)
+    get_repos(auth1, '../data/github_repos/github_repos.txt')
---
+++ tf-idf_model/github_tditf_model.py
@@ -0,0 +1,137 @@
+# https://medium.com/better-programming/introduction-to-gensim-calculating-text-similarity-9e8b55de342d
+# https://www.machinelearningplus.com/nlp/gensim-tutorial/
+# https://kavita-ganesan.com/gensim-word2vec-tutorial-starter-code/#.Xk-JKnVKg5k
+import csv
+import re
+import sys
+
+import jieba
+from gensim import corpora, models, similarities
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+def filter_sentence(sentence):
+    porter = PorterStemmer()
+    stopWords = set(stopwords.words('english'))
+    java_keywords = ["abstract", "assert", "boolean",
+                     "break", "byte", "case", "catch", "char", "class", "const",
+                     "continue", "default", "do", "double", "else", "extends", "false",
+                     "final", "finally", "float", "for", "goto", "if", "implements",
+                     "import", "instanceof", "int", "interface", "long", "native",
+                     "new", "null", "package", "private", "protected", "public",
+                     "return", "short", "static", "strictfp", "super", "switch",
+                     "synchronized", "this", "throw", "throws", "transient", "true",
+                     "try", "void", "volatile", "while"]
+
+    ret = ''
+    texts = word_tokenize(sentence)
+    for word in texts:
+        word = word.strip()
+        if word not in stopWords and word not in java_keywords:
+            stemmed_word = porter.stem(word)
+            if len(stemmed_word) > 2:
+                ret = ret + stemmed_word + " "
+    return ret
+
+
+def modify_comment(text):
+    # remove new lines with space
+    modified_text = text.replace("\n", " ")
+    # remove codes
+    modified_text = re.sub(r'```.+```', '', modified_text)
+    # remove non-ascii characters
+    modified_text = re.sub("([^\x00-\x7F])+", " ", modified_text)
+    # lower case
+    modified_text = modified_text.lower()
+    # remove special characters
+    modified_text = re.sub('[^A-Za-z]+', ' ', modified_text)
+    return modified_text
+
+
+if __name__ == '__main__':
+    issues = []
+    questions = []
+    answers = []
+    issue_links = []
+
+    originalIssues = []
+    originalQuestions = []
+    originalAnswers = []
+
+    csv.field_size_limit(sys.maxsize)
+    with open('../data/results/github_data.csv') as csvDataFile:
+        csvReader = csv.reader((line.replace('\0', '') for line in csvDataFile))
+        for row in csvReader:
+            if not row:
+                continue
+            if row[1] in issue_links:
+                continue
+            issues.append(filter_sentence(modify_comment(row[2])))
+            questions.append(filter_sentence(modify_comment(row[3])))
+            answers.append(filter_sentence(modify_comment(row[4])))
+            originalIssues.append(row[2])
+            originalQuestions.append(row[3])
+            originalAnswers.append(row[4])
+            issue_links.append(row[1])
+
+    print(len(issues))
+
+    texts_issues = [text.split() for text in issues]
+    dictionary_issue = corpora.Dictionary(texts_issues)
+    # print(dictionary_issue)
+    feature_cnt_issue = len(dictionary_issue.token2id)
+    corpus_issue = [dictionary_issue.doc2bow(text) for text in texts_issues]
+    tfidf_issue = models.TfidfModel(corpus=corpus_issue, normalize=True)
+    index_issue = similarities.SparseMatrixSimilarity(tfidf_issue[corpus_issue], num_features=feature_cnt_issue)
+
+    # print(dictionary_issue.token2id)
+
+    texts_questions = [text.split() for text in questions]
+    dictionary_questions = corpora.Dictionary(texts_questions)
+    # print(dictionary_questions)
+    feature_cnt_questions = len(dictionary_questions.token2id)
+    corpus_questions = [dictionary_questions.doc2bow(text) for text in texts_questions]
+    tfidf_questions = models.TfidfModel(corpus=corpus_questions, normalize=True)
+    index_questions = similarities.SparseMatrixSimilarity(tfidf_questions[corpus_questions],
+                                                          num_features=feature_cnt_questions)
+
+    result_folder = "results"
+    result_file = "tditf_github_data.csv"
+    comment_number = 0
+    unique_comments = 0
+    idx = 0
+    for question in questions:
+        kw_vector_issue_question = dictionary_issue.doc2bow(jieba.lcut(question))
+        sim_issue_question = index_issue[tfidf_issue[kw_vector_issue_question]]
+        unique = False
+
+        if sim_issue_question[idx] > 0.20:
+            unique_comments = unique_comments + 1
+
+            kw_vector_question_answer = dictionary_questions.doc2bow(jieba.lcut(answers[idx]))
+            sim_question_answer = index_questions[tfidf_questions[kw_vector_question_answer]]
+
+            sample_text = issues[idx] + "  " + questions[idx]
+            tfidf_values = dict(tfidf_issue[dictionary_issue.doc2bow(word_tokenize(sample_text))])
+            tfidf_values = sorted(tfidf_values.items(), reverse=True, key=lambda x: x[1])[:3]
+
+            top_terms = []
+            for value in tfidf_values:
+                top_terms.append(dictionary_issue[value[0]])
+            # print(top_terms)
+
+            sw = csv.writer(open('{0}/{1}'.format(result_folder, result_file), 'a'))
+            sw.writerow([
+                '{0}'.format(issue_links[idx]),
+                '{0}'.format(originalIssues[idx]),
+                '{0}'.format(originalQuestions[idx]),
+                '{0}'.format(sim_issue_question[idx]),
+                '{0}'.format(originalAnswers[idx]),
+                '{0}'.format(sim_question_answer[idx]),
+                '{0}'.format(top_terms)
+            ])
+
+        idx = idx + 1
+    print(unique_comments)
