57406908a96056dc548a50a25271318b3a923f14
Kostadin Damevski <kostadin@gmail.com>
2020-05-01 13:16:26 -0400

    now removing unclosed triple quotes

--- data_scraper/github_text_filter.py
+++ data_scraper/github_text_filter.py
@@ -15,6 +15,7 @@ def remove_newlines(text):
 
 def remove_triple_quotes(text):
     modified_text = re.sub(r'```.+```', '', text)
+    modified_text = re.sub(r'```.+', '', modified_text)
     return modified_text
 
 def filter_nontext(text):
--- data_scraper/test/test_text_filter.py
+++ data_scraper/test/test_text_filter.py
@@ -18,12 +18,18 @@ class TestRecPatterns(unittest.TestCase):
         text = filter(br)
         self.assertEqual(text.strip(), "something something  something")
 
-
     def test_triple_quotes_nl(self):
         br = 'something something ```code \n code \n' + \
              'code```\nsomething'
         text = filter(br)
         self.assertEqual(text.strip(), "something something  something")
+
+    def test_triple_quotes_eof(self):
+        br = 'something something ```code \n code \n' + \
+             'code\n' + \
+             'code'
+        text = filter(br)
+        self.assertEqual(text.strip(), "something something")
 
 if __name__ == "__main__":
     unittest.main()
