c429f93936288dc6e1cc4a2b33314533281e77f4
aciborowska <aga.ciborowska@gmail.com>
2020-05-15 13:51:46 -0400

    script to merge daatsets

---
+++ data/bug_reports/join_datasets.py
@@ -0,0 +1,30 @@
+import os
+
+
+def join_files(dpath, out_fpath):
+    lines = list()
+    header = None
+
+    for root, dirs, files in os.walk(dpath):
+        for file in files:
+            if 'github_data_20' in file:
+                print('Processing {0}'.format(file))
+                with open(os.path.join(root, file)) as f:
+                    line = f.readline()
+                    if header is None:
+                        header = line
+                    for line in f.readlines():
+                        lines.append(line.strip())
+    print('Done')
+    print('Save joined dataset to {0}'.format(out_fpath))
+    with open(out_fpath, 'w') as f:
+        f.write(header)
+        for line in lines:
+            f.write(line + '\n')
+    print('Done')
+
+
+if __name__ == '__main__':
+    dpath = '/Users/ciborowskaa/VCU/Research/BugReportQA/data/bug_reports'
+    out_fpath = os.path.join(dpath, 'github_dataset_partial.csv')
+    join_files(dpath, out_fpath)
