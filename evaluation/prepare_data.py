1e799db80a7fc18744b0cb0638932bb3c21b0a0e
aciborowska <aga.ciborowska@gmail.com>
2020-05-19 14:53:41 -0400

    fix for ranking data

--- evaluation/prepare_data.py
+++ evaluation/prepare_data.py
@@ -1,14 +1,15 @@
 import click
 import sys
+import pandas as pd
 
 
 @click.command()
-@click.command('--input-refs', required=True,
+@click.option('--input-refs', required=True,
                help='File with correct questions. When method=ranking, the file contains also ranked questions.')
-@click.command('--input-questions', help='File with generated/ranked questions. Use only when method=GAN.')
-@click.command('--output-refs', required=True)
-@click.command('--output-questions', required=True)
-@click.command('--method', required=True, type=click.Choice(['GAN', 'ranking']))
+@click.option('--input-questions', help='File with generated/ranked questions. Use only when method=GAN.')
+@click.option('--output-refs', required=True)
+@click.option('--output-questions', required=True)
+@click.option('--method', required=True, type=click.Choice(['GAN', 'ranking']))
 def run(*args, **kwargs):
     method = kwargs['method']
     in_refs = kwargs['input_refs']
@@ -42,14 +43,10 @@ def data4GAN(in_refs, out_refs, in_questions, out_questions):
 def data4ranking(in_refs, out_refs, out_questions):
     new_refs = open(out_refs, 'w')
     new_q = open(out_questions, 'w')
-    with open(in_refs, 'r') as f:
-        # skip header
-        # id, <10 predictions ordered by the numbers in the output file>, correct question
-        f.readline()
-        for line in f.readlines():
-            values = line.strip().split(',')
-            new_refs.write(values[11] + '\n')
-            new_q.write(values[1] + '\n')
+    df = pd.read_csv(in_refs)
+    for index, row in df.iterrows():
+        new_refs.write(row['q1'] + '\n')
+        new_q.write(row['correct question'] + '\n')
 
     new_refs.close()
     new_q.close()
