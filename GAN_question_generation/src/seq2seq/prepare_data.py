0de032740ce35e9661304040e945ac8bb38f50f8
aciborowska <aga.ciborowska@gmail.com>
2020-05-20 14:38:19 -0400

    restore prev splitting. it did nothing anyway

--- GAN_question_generation/src/seq2seq/prepare_data.py
+++ GAN_question_generation/src/seq2seq/prepare_data.py
@@ -8,7 +8,7 @@ from constants import *
 
 # Return a list of indexes, one for each word in the sentence, plus EOS
 def prepare_sequence(seq, word2index, max_len):
-    sequence = [word2index[w] if w in word2index else word2index['<unk>'] for w in split_text(seq)[:max_len - 1]]
+    sequence = [word2index[w] if w in word2index else word2index['<unk>'] for w in seq.split(' ')[:max_len - 1]]
     sequence.append(word2index[EOS_token])
     length = len(sequence)
     sequence += [word2index[PAD_token]] * (max_len - len(sequence))
@@ -17,11 +17,11 @@ def prepare_sequence(seq, word2index, max_len):
 
 def prepare_pq_sequence(post_seq, ques_seq, word2index, max_post_len, max_ques_len):
     p_sequence = [word2index[w] if w in word2index else word2index['<unk>'] for w in
-                  split_text(post_seq)[:(max_post_len - 1)]]
+                  post_seq.split(' ')[:(max_post_len - 1)]]
     p_sequence.append(word2index[EOP_token])
     p_sequence += [word2index[PAD_token]] * (max_post_len - len(p_sequence))
     q_sequence = [word2index[w] if w in word2index else word2index['<unk>'] for w in
-                  split_text(ques_seq)[:(max_ques_len - 1)]]
+                  ques_seq.split(' ')[:(max_ques_len - 1)]]
     q_sequence.append(word2index[EOS_token])
     q_sequence += [word2index[PAD_token]] * (max_ques_len - len(q_sequence))
     sequence = p_sequence + q_sequence
@@ -59,17 +59,3 @@ def preprocess_data(triples, word2index, max_post_len, max_ques_len, max_ans_len
 
     return id_seqs, post_seqs, post_lens, ques_seqs, ques_lens, \
            post_ques_seqs, post_ques_lens, ans_seqs, ans_lens
-
-
-def split_text(text):
-    line = str(text).lower().strip()
-
-    # punctuation removal
-    line_filtered = ''
-    for c in line:
-        if c in '!@$%^&*()[]{};:,./<>?\|`~-=':
-            line_filtered += ' '
-        else:
-            line_filtered += c
-
-    return line_filtered.split(' ')
