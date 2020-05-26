0ceac34f0cdef9ab4408943277cf15062d95c994
aciborowska <aga.ciborowska@gmail.com>
2020-05-26 14:41:02 -0400

    small refactoring

--- data_scraper/github_apis_v3.py
+++ data_scraper/github_apis_v3.py
@@ -4,66 +4,49 @@ import requests
 
 
 def get_comments(url, headers):
-    response = requests.get(url, headers=headers)
-    max_try = 20
-    while response.status_code != 200:
-        if response.status_code == 404:
-            print("Comments, Bad response code 404 Not Found, returning...", url, time.ctime())
-            return None
-        elif response.status_code == 410:
-            print("Comments, Bad response code: 410 Requested page is no longer available. Returning...", url,
-                  time.ctime())
-            return None
-        elif response.status_code == 301:
-            print("Comments, Bad response code: 301 Requested page permanently moved. Returning...", url, time.ctime())
-            return None
-
-        if max_try < 0:
-            break
-        max_try = max_try - 1
-        if response.status_code == 401:
-            print("Comments, Bad response code 401 Bad Credentials, sleeping for 3 minutes...", url, time.ctime())
-        else:
-            print("Comments, Bad response code:", response.status_code, "sleeping for 3 minutes....", url, time.ctime())
-        time.sleep(180)
-        response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    return execute_query(url, headers, 'Comments')
 
 
 def get_an_issue(repo, issue_id, headers):
     url = "https://api.github.com/repos/{repo}/issues/{issue_id}"
     url = url.format(repo=repo, issue_id=issue_id)
 
+    return execute_query(url, headers, 'Issues')
+
+
+def execute_query(url, headers, query_type):
     response = requests.get(url, headers=headers)
     max_try = 20
     while response.status_code != 200:
         if response.status_code == 404:
-            print("Issues, Bad response code: 404 Not Found. Returning...", url, time.ctime())
+            print("{2} - {0}. Bad response code 404 Not Found for {1}. Return None.".format(query_type, url,
+                                                                                            time.ctime()))
             return None
         elif response.status_code == 410:
-            print("Issues, Bad response code: 410 Requested page is no longer available. Returning...", url,
-                  time.ctime())
+            print(
+                "{2} - {0}. Bad response code: 410 Requested page is no longer available for {1}. Return None.".format(
+                    query_type, url, time.ctime()))
             return None
         elif response.status_code == 301:
-            print("Issues, Bad response code: 301 Requested page permanently moved. Returning...", url, time.ctime())
+            print("{2} - {0}. Bad response code: 301 Requested page permanently moved for {1}. Return None.".format(
+                query_type, url, time.ctime()))
             return None
 
         if max_try < 0:
             break
         max_try = max_try - 1
         if response.status_code == 401:
-            print("Issues, Bad response code 401 Bad Credentials, sleeping for 3 minutes...", url, time.ctime())
+            print("{2} - {0}. Bad response code 401 Bad Credentials for {1}. Sleep for 3 minutes...".format(query_type,
+                                                                                                            url,
+                                                                                                            time.ctime()))
         else:
-            print("Issues, Bad response code:", response.status_code, "sleeping for 3 minutes....", url, time.ctime())
+            print("{2} - {0}. Bad response code: {3} for {1}. Sleeping for 3 minutes....".format(query_type, url,
+                                                                                                 time.ctime(),
+                                                                                                 response.status_code))
         time.sleep(180)
         response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        _json = response.json()
-        return _json
+        return response.json()
     else:
         return None
