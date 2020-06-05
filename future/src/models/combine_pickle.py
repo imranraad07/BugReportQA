d31035f8740a4d5b8d6d60b4ac2ba88d4a28d517
aciborowska <aga.ciborowska@gmail.com>
2020-06-05 14:49:31 -0400

    removed old model files

--- future/src/models/combine_pickle.py
+++
@@ -1,9 +0,0 @@
-import sys
-import cPickle as p
-
-if __name__ == "__main__":
-	askubuntu = p.load(open(sys.argv[1], 'rb'))
-	unix = p.load(open(sys.argv[2], 'rb'))
-	superuser = p.load(open(sys.argv[3], 'rb'))
-	combined = unix + superuser + askubuntu
-	p.dump(combined, open(sys.argv[4], 'wb'))
