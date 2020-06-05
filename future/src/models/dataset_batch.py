fe305a19e43449abbf3ac74161fc238bca9b86e5
aciborowska <aga.ciborowska@gmail.com>
2020-06-05 10:55:26 -0400

    evpi for batch of random size

---
+++ future/src/models/dataset_batch.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from data_generation import preprocessing as pp
+import calculator as calc
+
+
+def get_datasets(post_tsv, qa_tsv, word2index, batch_size=256, shuffle=True, max_post_len=300, max_q_len=100,
+                 max_a_len=100):
+    train_dataset = GithubDataset(post_tsv, qa_tsv, word2index, train=True, max_post_len=max_post_len,
+                                  max_q_len=max_q_len, max_a_len=max_a_len)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0)
+
+    test_dataset = GithubDataset(post_tsv, qa_tsv, word2index, train=False, max_post_len=max_post_len,
+                                 max_q_len=max_q_len, max_a_len=max_a_len)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0)
+
+    return train_loader, test_loader
+
+
+class GithubDataset(Dataset):
+
+    def __init__(self, post_tsv, qa_tsv, word2index, train, max_post_len, max_q_len, max_a_len):
+        self.word2index = word2index
+        self.max_post_len = max_post_len
+        self.max_q_len = max_q_len
+        self.max_a_len = max_a_len
+        self.dataset = self._build_dataset(post_tsv, qa_tsv, train)
+
+    def _build_dataset(self, post_tsv, qa_tsv, train):
+        calculator = calc.Calculator()
+        posts = pd.read_csv(post_tsv, sep='\t')[:13]
+        qa = pd.read_csv(qa_tsv, sep='\t')[:13]
+        data = {'postid': list(),
+                'post_origin': list(),
+                'question_origin': list(),
+                'answer_origin': list(),
+                'label': list(),
+                'utility': list()}
+
+        for idx, row in posts.iterrows():
+            # TODO: this should be covered when building dataset
+            if str(row['post']) == 'nan':
+                continue
+
+            postid = row['postid']
+            post = row['title'] + ' ' + row['post']
+            for i in range(1, 11):
+                question = qa.iloc[idx]['q' + str(i)]
+                answer = qa.iloc[idx]['a' + str(i)]
+                utility = calculator.utility(answer, post)
+
+                data = self._add_values(data, i, postid, post, question, answer, utility)
+
+        dataset = pd.DataFrame(data)
+        dataset = self._preprocess(dataset)
+
+        instances_no = len(dataset) / 10
+        train_instances = int(instances_no * 0.9)
+        if train is True:
+            dataset = dataset[:(train_instances * 10)]
+        else:
+            dataset = dataset[(train_instances * 10):].reset_index(drop=True)
+        return dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = self.dataset.iloc[idx]
+        sample = self._to_dict(sample)
+        return sample
+
+    def _to_dict(self, sample):
+        new_sample = {'postid': sample['postid'],
+                      'post': sample['post'],
+                      'post_len': sample['post_len'],
+                      'question': sample['question'],
+                      'q_len': sample['q_len'],
+                      'a_len': sample['a_len'],
+                      'answer': sample['answer'],
+                      'post_origin': sample['post_origin'],
+                      'question_origin': sample['question_origin'],
+                      'answer_origin': sample['answer_origin'],
+                      'utility': sample['utility'],
+                      'label': sample['label']}
+        return new_sample
+
+    def _add_values(self, data, index, postid, post, question, answer, utility):
+        data['postid'].append(postid)
+        data['post_origin'].append(post)
+        data['answer_origin'].append(answer)
+        data['question_origin'].append(question)
+        data['utility'].append(utility)
+        if index == 1:
+            data['label'].append(1)
+        else:
+            data['label'].append(0)
+        return data
+
+    def _preprocess(self, dataset):
+        dataset['post'] = dataset.post_origin.apply(self._clear_text)
+        dataset['post'] = dataset.post.apply(self._word2index)
+        dataset['post_len'] = dataset.post.apply(lambda x: self.max_post_len if len(x) > self.max_post_len else len(x))
+        dataset['post'] = dataset.post.apply(self._padding_post)
+        dataset['post'] = dataset.post.apply(self._to_tensor)
+        dataset['post_len'] = dataset.post_len.apply(self._to_tensor)
+
+        dataset['answer'] = dataset.answer_origin.apply(self._clear_text)
+        dataset['answer'] = dataset.answer.apply(self._word2index)
+        dataset['a_len'] = dataset.answer.apply(lambda x: self.max_a_len if len(x) > self.max_a_len else len(x))
+        dataset['answer'] = dataset.answer.apply(self._padding_answer)
+        dataset['answer'] = dataset.answer.apply(self._to_tensor)
+        dataset['a_len'] = dataset.a_len.apply(self._to_tensor)
+
+        dataset['question'] = dataset.question_origin.apply(self._clear_text)
+        dataset['question'] = dataset.question.apply(self._word2index)
+        dataset['q_len'] = dataset.question.apply(lambda x: self.max_q_len if len(x) > self.max_q_len else len(x))
+        dataset['question'] = dataset.question.apply(self._padding_question)
+        dataset['question'] = dataset.question.apply(self._to_tensor)
+        dataset['q_len'] = dataset.q_len.apply(self._to_tensor)
+
+        return dataset
+
+    # all data transformations
+    def _clear_text(self, text):
+        return pp.clear_text(text, keep_punctuation=True)
+
+    def _word2index(self, text):
+        return [self.word2index[w].index for w in text.split() if w in self.word2index]
+
+    def _to_tensor(self, value):
+        return torch.tensor(value, dtype=torch.long)
+
+    def _padding_post(self, values):
+        return self._padding(values, self.max_post_len)
+
+    def _padding_answer(self, values):
+        return self._padding(values, self.max_a_len)
+
+    def _padding_question(self, values):
+        return self._padding(values, self.max_q_len)
+
+    def _padding(self, values, limit):
+        padded = np.zeros((limit,), dtype=np.int64)
+        if len(values) > limit:
+            padded[:] = values[:limit]
+        else:
+            padded[:len(values)] = values
+        return padded
---
+++ future/src/models/evpi_batch.py
@@ -0,0 +1,225 @@
+import numpy as np
+
+import torch.nn as nn
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+import gensim
+from gensim.scripts.glove2word2vec import glove2word2vec
+from scipy import spatial
+
+import dataset_batch as dataset
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+CUDA = False
+
+"""
+TODO Big things:
+1. Done: Evaluation on test set
+2. Done: Ranking
+3. Done: make sure ids in the dataset are properly divided! One tsv row = 10 data points (p, q_i, a_i))
+4. Padding for batch processing - do it in a new file; it requires more modifications
+
+TODO Small things:
+1. Make sure diff computation makes sense.
+2. Done: Move utility calculator as a part of dataset building set - OB is not changing.
+
+Nice to have:
+1. Evaluation on validation set and saving model that performs best on validation set.
+   We can use that model later on for evaluation with test set.
+"""
+
+
+class EvpiModel(nn.Module):
+
+    def __init__(self, weights, hidden_dim=256):
+        super(EvpiModel, self).__init__()
+
+        # layer 1 - embeddings
+        weights = torch.FloatTensor(weights)
+        self.emb_layer = nn.Embedding.from_pretrained(weights)
+        self.emb_layer.requires_grad = False
+
+        # layer 2 - LSTMs
+        self.p_lstm = nn.LSTM(input_size=self.emb_layer.embedding_dim, hidden_size=hidden_dim, batch_first=True)
+        self.q_lstm = nn.LSTM(input_size=self.emb_layer.embedding_dim, hidden_size=hidden_dim, batch_first=True)
+
+        # layer 3 - dense layer
+        self.layer1 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
+        self.layer2 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
+        self.layer3 = nn.Linear(2 * hidden_dim, self.emb_layer.embedding_dim)
+        self.layer4 = nn.Linear(self.emb_layer.embedding_dim, self.emb_layer.embedding_dim)
+
+    def forward(self, post, post_lengths, question, question_lengths):
+        # sort data
+        post, post_lengths, post_sorted_id = self.sort_batch(post, post_lengths)
+        question, question_lengths, question_sorted_id = self.sort_batch(question, question_lengths)
+
+        # process posts
+        p_emb_out = self.emb_layer(post)
+        p_emb_out = pack_padded_sequence(p_emb_out, post_lengths, batch_first=True)
+        p_lstm_out, _ = self.p_lstm(p_emb_out)
+        p_lstm_out, _ = pad_packed_sequence(p_lstm_out, batch_first=True)
+        p_mean = p_lstm_out.mean(dim=1)
+
+        # process questions
+        q_emb_out = self.emb_layer(question)
+        q_emb_out = pack_padded_sequence(q_emb_out, question_lengths, batch_first=True)
+        q_lstm_out, _ = self.q_lstm(q_emb_out)
+        q_lstm_out, _ = pad_packed_sequence(q_lstm_out, batch_first=True)
+        q_mean = q_lstm_out.mean(dim=1)
+
+        # restore ordering
+        p_mean, post_lengths = self.restore_ordering(p_mean, post_lengths, post_sorted_id)
+        q_mean, question_lengths = self.restore_ordering(q_mean, question_lengths, question_sorted_id)
+
+        # linear layers
+        lstm_avg = torch.cat((p_mean, q_mean), 1)
+        x = F.relu(self.layer1(lstm_avg))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        answer_representation = self.layer4(x)
+        return answer_representation
+
+    def sort_batch(self, data, seq_len):
+        """ Sort the data (B, T, D) and sequence lengths """
+        sorted_seq_len, sorted_idx = seq_len.sort(0, descending=True)
+        sorted_data = data[sorted_idx]
+        return sorted_data, sorted_seq_len, sorted_idx
+
+    def restore_ordering(self, data, seq_len, sorted_idx):
+        """ Restore original ordering of data based on sorted ids """
+        sorted_sorted_id, initial_id = sorted_idx.sort(0, descending=False)
+        sorted_data = data[initial_id]
+        sorted_len = seq_len[initial_id]
+        return sorted_data, sorted_len
+
+
+def get_device():
+    if CUDA is True:
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    else:
+        device = torch.device('cpu')
+    return device
+
+
+def compute_a_cap(answer, w2v_model):
+    answer = answer.numpy()
+    a_cap = np.ndarray((answer.shape[0], w2v_model.vectors.shape[1]))
+    for idx, tensor in enumerate(answer):
+        words = [w2v_model.index2word[index] for index in tensor]
+        a_cap[idx] = np.mean(w2v_model[words], axis=0)
+    return torch.tensor(a_cap, dtype=torch.float)
+
+
+def cosine_similarity(v1, v2):
+    return 1 - spatial.distance.cosine(v1, v2)
+
+
+def run_evaluation(net, device, w2v_model, test_loader):
+    results = {}
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            if CUDA and torch.cuda.is_available():
+                posts, post_len, questions, q_len, a_cap = data['post'].to(device), data['post_len'].to(device), \
+                                                           data['question'].to(device), data['q_len'].to(device), \
+                                                           a_cap.to(device)
+            else:
+                posts = data['post']
+                questions = data['question']
+                post_len = data['post_len']
+                q_len = data['q_len']
+
+            postid = data['postid']
+            answers = data['answer']
+            utility = data['utility'].numpy()
+            posts_origin = data['post_origin']
+            answers_origin = data['answer_origin']
+            questions_origin = data['question_origin']
+
+            outputs = net(posts, post_len, questions, q_len)
+            if device.type != 'cpu':
+                outputs = outputs.cpu()
+
+            outputs = outputs.numpy()
+
+            a_cap = compute_a_cap(answers, w2v_model).numpy()
+
+            for i in range(0, test_loader.batch_size):
+                sim = cosine_similarity(a_cap[i], outputs[i])
+
+                score = sim * utility[i]
+                if postid[i] not in results:
+                    results[postid[i]] = (posts_origin[i], list())
+                results[postid[i]][1].append((score, questions_origin[i], answers_origin[i]))
+    return results
+
+
+def create_ranking(output_file, results):
+    with open(output_file, 'w') as f:
+        f.write('postid,post,' + ','.join(['q{0},a{0}'.format(i) for i in range(1, 11)]) + '\n')
+        for postid in results:
+            post, values = results[postid]
+            f.write('{0},{1},'.format(postid, post.replace(',', ' ')))
+
+            values = sorted(values, key=lambda x: x[0], reverse=True)
+            for score, question, answer in values:
+                f.write('{0},{1},'.format(question.replace(',', ' '), answer.replace(',', ' ')))
+            f.write('\n')
+
+
+def evpi(vector_fpath, post_tsv, qa_tsv, ranking_output, n_epochs):
+    glove2word2vec(vector_fpath, '../../embeddings_damevski/w2v_vectors.txt')
+    w2v_model = gensim.models.KeyedVectors.load_word2vec_format('../../embeddings_damevski/w2v_vectors.txt')
+    net = EvpiModel(w2v_model.vectors)
+
+    device = get_device()
+    net.to(device)
+    print('Running on {0}'.format(device))
+
+    train_loader, test_loader = dataset.get_datasets(post_tsv, qa_tsv, w2v_model.vocab, batch_size=2)
+
+    loss_function = nn.SmoothL1Loss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001)
+
+    for epoch in range(n_epochs):
+        loss_sum = 0.0
+        for i, data in enumerate(train_loader):
+            # compute a_cap and send it to device so it can be used for back propagation
+            answers = data['answer']
+            a_cap = compute_a_cap(answers, w2v_model)
+
+            if CUDA and torch.cuda.is_available():
+                posts, post_len, questions, q_len, a_cap = data['post'].to(device), data['post_len'].to(device), \
+                                                           data['question'].to(device), data['q_len'].to(device), \
+                                                           a_cap.to(device)
+            else:
+                posts = data['post']
+                questions = data['question']
+                post_len = data['post_len']
+                q_len = data['q_len']
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(posts, post_len, questions, q_len)
+
+            loss = loss_function(outputs, a_cap)
+            loss.backward()
+            optimizer.step()
+            loss_sum += loss.item()
+
+    results = run_evaluation(net, device, w2v_model, test_loader)
+    create_ranking(ranking_output, results)
+
+
+if __name__ == '__main__':
+    evpi('../../embeddings_damevski/vectors.txt',
+         '../../data/github_partial_2008-2013_part1_small/post_data.tsv',
+         '../../data/github_partial_2008-2013_part1_small/qa_data.tsv',
+         'ranking.csv',
+         1)
