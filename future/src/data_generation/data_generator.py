a8dc1dc746a4f3efc4be646c58355cfad47bf120
aciborowska <aga.ciborowska@gmail.com>
2020-06-07 15:41:04 -0400

    clean up data generator. compute ob in data_generator

--- future/src/data_generation/data_generator.py
+++ future/src/data_generation/data_generator.py
@@ -1,10 +1,13 @@
 import argparse
 import csv
 import os
+import sys
+
+sys.path.append(os.path.abspath('../pattern_classification'))
 
 from preprocessing import clear_text
 
-from parse import *
+import compute_ob as ob
 from post_ques_ans_generator import *
 
 
@@ -21,14 +24,6 @@ def get_similar_docs(lucene_similar_docs):
     return similar_docs
 
 
-def generate_docs_for_lucene(post_ques_answers, posts, output_dir):
-    for postId in post_ques_answers:
-        f = open(os.path.join(output_dir, str(postId) + '.txt'), 'w')
-        content = ' '.join(posts[postId].title).encode('utf-8') + ' ' + ' '.join(posts[postId].body).encode('utf-8')
-        f.write(content)
-        f.close()
-
-
 def generate_docs_for_lucene_github(titles, posts, output_dir, post_ids):
     for postId in range(0, len(posts)):
         f = open(os.path.join(output_dir, post_ids[postId] + '.txt'), 'w')
@@ -38,35 +33,7 @@ def generate_docs_for_lucene_github(titles, posts, output_dir, post_ids):
         f.close()
 
 
-def create_tsv_files(post_data_tsv, qa_data_tsv, post_ques_answers, lucene_similar_posts):
-    lucene_similar_posts = get_similar_docs(lucene_similar_posts)
-    similar_posts = {}
-    for line in lucene_similar_posts.readlines():
-        splits = line.strip('\n').split()
-        if len(splits) < 11:
-            continue
-        postId = splits[0]
-        similar_posts[postId] = splits[1:11]
-    post_data_tsv_file = open(post_data_tsv, 'w')
-    post_data_tsv_file.write('postid\ttitle\tpost\n')
-    qa_data_tsv_file = open(qa_data_tsv, 'w')
-    qa_data_tsv_file.write('postid\tq1\tq2\tq3\tq4\tq5\tq6\tq7\tq8\tq9\tq10\ta1\ta2\ta3\ta4\ta5\ta6\ta7\ta8\ta9\ta10\n')
-    print
-    len(similar_posts)
-    for postId in similar_posts:
-        post_data_tsv_file.write('%s\t%s\t%s\n' % (postId, \
-                                                   ' '.join(post_ques_answers[postId].post_title), \
-                                                   ' '.join(post_ques_answers[postId].post)))
-        line = postId
-        for i in range(10):
-            line += '\t%s' % ' '.join(post_ques_answers[similar_posts[postId][i]].question_comment)
-        for i in range(10):
-            line += '\t%s' % ' '.join(post_ques_answers[similar_posts[postId][i]].answer)
-        line += '\n'
-        qa_data_tsv_file.write(line)
-
-
-def create_tsv_files_github(post_data_tsv, qa_data_tsv, post_ids, post_titles, post_texts, post_questions,
+def create_tsv_files_github(post_data_tsv, qa_data_tsv, utility_data_tsv, post_titles, post_texts, post_questions,
                             post_answers, lucene_similar_posts):
     lucene_similar_posts = get_similar_docs(lucene_similar_posts)
     similar_posts = {}
@@ -87,6 +54,8 @@ def create_tsv_files_github(post_data_tsv, qa_data_tsv, post_ids, post_titles, p
     for postId in similar_posts:
         with open(post_data_tsv, 'a') as out_file:
             tsv_writer = csv.writer(out_file, delimiter='\t')
+            if str(post_texts[postId]) == '':
+                continue
             tsv_writer.writerow(
                 [postId, post_titles[postId], post_texts[postId]])
 
@@ -103,127 +72,48 @@ def create_tsv_files_github(post_data_tsv, qa_data_tsv, post_ids, post_titles, p
                 tsv_writer = csv.writer(out_file, delimiter='\t')
                 tsv_writer.writerow(row_val)
 
+    ob.compute_utilities(post_data_tsv, qa_data_tsv, utility_data_tsv)
+
 
 def main(args):
-    if args.site_name == "github":
-        print
-        'Parsing github posts...'
-        csv.field_size_limit(sys.maxsize)
-        post_ids = []
-        post_titles = {}
-        post_texts = {}
-        post_questions = {}
-        post_answers = {}
-        idx = 0
-        with open(args.github_csv) as csvDataFile:
-            csvReader = csv.reader((line.replace('\0', '') for line in csvDataFile))
-            next(csvReader)
-            for row in csvReader:
-                post_ids.append(row[2])
-                post_titles[row[2]] = (row[3].partition('\n')[0])
-                post_texts[row[2]] = (row[3].partition('\n')[-1])
-                post_questions[row[2]] = (row[4])
-                post_answers[row[2]] = (row[5])
-                idx = idx + 1
-        # print idx
-        generate_docs_for_lucene_github(post_titles, post_texts, args.lucene_docs_dir, post_ids)
-        os.system('cd %s && sh run_lucene.sh %s' % (args.lucene_dir, os.path.dirname(args.post_data_tsv)))
+    print('Parsing github posts...')
+    csv.field_size_limit(sys.maxsize)
+    post_ids = []
+    post_titles = {}
+    post_texts = {}
+    post_questions = {}
+    post_answers = {}
+    idx = 0
+    with open(args.github_csv) as csvDataFile:
+        csvReader = csv.reader((line.replace('\0', '') for line in csvDataFile))
+        next(csvReader)
+        for row in csvReader:
+            post_ids.append(row[2])
+            post_titles[row[2]] = (row[3].partition('\n')[0])
+            post_texts[row[2]] = (row[3].partition('\n')[-1])
+            post_questions[row[2]] = (row[4])
+            post_answers[row[2]] = (row[5])
+            idx = idx + 1
+    # print idx
+    generate_docs_for_lucene_github(post_titles, post_texts, args.lucene_docs_dir, post_ids)
+    os.system('cd %s && sh run_lucene.sh %s' % (args.lucene_dir, os.path.dirname(args.post_data_tsv)))
 
-        create_tsv_files_github(args.post_data_tsv, args.qa_data_tsv, post_ids, post_titles, post_texts,
-                                post_questions, post_answers, args.lucene_similar_posts)
-
-    else:
-        start_time = time.time()
-        print
-        'Parsing posts...'
-        post_parser = PostParser(args.posts_xml)
-        post_parser.parse()
-        posts = post_parser.get_posts()
-        print
-        'Size: ', len(posts)
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        start_time = time.time()
-        print
-        'Parsing posthistories...'
-        posthistory_parser = PostHistoryParser(args.posthistory_xml)
-        posthistory_parser.parse()
-        posthistories = posthistory_parser.get_posthistories()
-        print
-        'Size: ', len(posthistories)
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        start_time = time.time()
-        print
-        'Parsing question comments...'
-        comment_parser = CommentParser(args.comments_xml)
-        comment_parser.parse_all_comments()
-        question_comments = comment_parser.get_question_comments()
-        all_comments = comment_parser.get_all_comments()
-        print
-        'Size: ', len(question_comments)
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        start_time = time.time()
-        print
-        'Loading vocab'
-        vocab = p.load(open(args.vocab, 'rb'))
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        start_time = time.time()
-        print
-        'Loading word_embeddings'
-        word_embeddings = p.load(open(args.word_embeddings, 'rb'))
-        word_embeddings = np.asarray(word_embeddings, dtype=np.float32)
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        start_time = time.time()
-        print
-        'Generating post_ques_ans...'
-        post_ques_ans_generator = PostQuesAnsGenerator()
-        post_ques_answers = post_ques_ans_generator.generate(posts, question_comments, all_comments, posthistories,
-                                                             vocab,
-                                                             word_embeddings)
-        print
-        'Size: ', len(post_ques_answers)
-        print
-        'Done! Time taken ', time.time() - start_time
-        print
-
-        generate_docs_for_lucene(post_ques_answers, posts, args.lucene_docs_dir)
-        os.system('cd %s && sh run_lucene.sh %s' % (args.lucene_dir, os.path.dirname(args.post_data_tsv)))
-
-        create_tsv_files(args.post_data_tsv, args.qa_data_tsv, post_ques_answers, args.lucene_similar_posts)
+    create_tsv_files_github(args.post_data_tsv, args.qa_data_tsv, args.utility_data_tsv, post_titles, post_texts,
+                            post_questions, post_answers, args.lucene_similar_posts)
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(sys.argv[0])
-    argparser.add_argument("--posts_xml", type=str)
-    argparser.add_argument("--comments_xml", type=str)
-    argparser.add_argument("--posthistory_xml", type=str)
     argparser.add_argument("--lucene_dir", type=str)
     argparser.add_argument("--lucene_docs_dir", type=str)
     argparser.add_argument("--lucene_similar_posts", type=str)
     argparser.add_argument("--word_embeddings", type=str)
     argparser.add_argument("--vocab", type=str)
     argparser.add_argument("--no_of_candidates", type=int, default=10)
-    argparser.add_argument("--site_name", type=str, default='github')
     argparser.add_argument("--post_data_tsv", type=str)
     argparser.add_argument("--qa_data_tsv", type=str)
+    argparser.add_argument("--utility_data_tsv", type=str)
     argparser.add_argument("--github_csv", type=str)
     args = argparser.parse_args()
-    print
-    args
-    print
-    ""
+    print(args)
     main(args)
--- future/src/data_generation/run_data_generator_github.sh
+++ future/src/data_generation/run_data_generator_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATA_DIR=data
-SITE_NAME=github
+SITE_NAME=github_20K
 
 SCRIPTS_DIR=src/data_generation
 LUCENE_DIR=lucene
@@ -19,7 +19,7 @@ mkdir -p $DATA_DIR/$SITE_NAME/ques_docs
 python $SCRIPTS_DIR/data_generator.py   --lucene_dir $LUCENE_DIR \
                                         --lucene_docs_dir $DATA_DIR/$SITE_NAME/post_docs \
                                         --lucene_similar_posts $DATA_DIR/$SITE_NAME/lucene_similar_posts.txt \
-                                        --site_name $SITE_NAME \
                                         --post_data_tsv $DATA_DIR/$SITE_NAME/post_data.tsv \
                                         --qa_data_tsv $DATA_DIR/$SITE_NAME/qa_data.tsv \
-                                        --github_csv $DATA_DIR/$SITE_NAME/github_data.csv
+                                        --utility_data_tsv $DATA_DIR/$SITE_NAME/utility_data.tsv \
+                                        --github_csv $DATA_DIR/$SITE_NAME/dataset.csv
