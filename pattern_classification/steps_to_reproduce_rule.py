e7a57e5f4f22e418f9ca098fb7d41bfa7fc8b6f2
imranm3 <imranm3@egr-u-cs-kd-02.rams.adp.vcu.edu>
2020-06-09 05:13:47 -0400

    sentence level rule added for steps to reproduce

--- pattern_classification/steps_to_reproduce_rule.py
+++ pattern_classification/steps_to_reproduce_rule.py
@@ -5,6 +5,7 @@ import pandas as pd
 from spacy.matcher import Matcher
 import re
 
+
 # s2r_labels = ["how to reproduce", "what i have tried", "to replicate", "steps to reproduce", "steps to recreate"]
 
 
@@ -28,11 +29,25 @@ def setup_p_sr_labeled_paragraph(doc):
     return False
 
 
+# S_SR_CODE_REF
+def setup_s_sr_code_ref(doc):
+    doc = doc.lower()
+    match = re.search(
+        '.*(code snippet|sample example|live example|test case)(.+?)(attached|below|provided|here|enclosed|following)(.+?)',
+        doc)
+    if match is not None:
+        return True
+    return False
+
+
 if __name__ == '__main__':
 
     issues = pd.read_csv('../data/datasets/dataset.csv')
 
     print("Total issues:", len(issues["post"]))
+    nlp = spacy.load("en_core_web_sm")
+    # matcher = Matcher(nlp.vocab, validate=True)
+    # setup_s_sr_code_ref(matcher)
 
     issue_matches = []
     s2r_list = []
@@ -46,8 +61,22 @@ if __name__ == '__main__':
             s2r_list.append(paragraph)
             count = count + 1
             print(count, index, issue["issue_link"])
+        # # sentence matching steps to reproduce
+        if not matched:
+            s2r_sent = ""
+            for sentence in nlp(issue["post"]).sents:
+                sent = sentence.text.strip()
+                flag = setup_s_sr_code_ref(sent)
+                if flag:
+                    matched = True
+                    s2r_sent = s2r_sent + " " + sent
+            if matched:
+                s2r_list.append(s2r_sent)
+                count = count + 1
+                print(count, index, issue["issue_link"])
         issue_matches.append(matched)
 
+    print(count)
     s2r_issues = issues[issue_matches]
     assert (len(s2r_issues) == len(s2r_list))
     s2r_issues = s2r_issues.assign(STR=s2r_list)
--- pattern_classification/test_steps_to_reproduce.py
+++ pattern_classification/test_steps_to_reproduce.py
@@ -13,16 +13,6 @@ class TestRecPatterns(unittest.TestCase):
         self.matcher = Matcher(self.nlp.vocab, validate=True)
         return unittest.TestCase.setUp(self)
 
-    # def test_basecase_s_sr_when_after(self):
-    #     s2r.setup_s_sr_when_after(self.matcher)
-    #     sent = "when I press the  publish  button on the facebook interface after entering some text about a post_ I get an empty window with only an  x  in the upper right-hand corner and the app just sits there."
-    #     sent_nlp = self.nlp(sent)
-    #     for token in sent_nlp:
-    #         print(token.pos_, token.text, token.lemma_)
-    #     matches = self.matcher(sent_nlp)
-    #     self.assertTrue(len(matches) == 1)
-
-
     def test_basecase_p_s2r_labeled_list(self):
         doc = "The problem occurs when I tried this. steps to reproduce:  1. Create a bar chart" \
               " 2. Open chart builder " \
@@ -50,6 +40,10 @@ class TestRecPatterns(unittest.TestCase):
               " Open chart builder. " \
               " click filters. preview."
         self.assertTrue(s2r.setup_p_sr_labeled_paragraph(doc) == True)
+
+    def test_basecase_s_sr_code_ref(self):
+        doc = "the code snippet below opens a shell that goes across multiple monitors when there are multiple monitors installed."
+        self.assertTrue(s2r.setup_s_sr_code_ref(doc) == True)
 
 
 if __name__ == "__main__":
