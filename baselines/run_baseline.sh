f971df0c54c905c8561a3b9a60daeaefe59cf567
imranm3 <imranm3@egr-u-cs-kd-02.rams.adp.vcu.edu>
2020-07-23 14:52:13 -0400

    baseline implementation added

--- baselines/run_baseline.sh
+++ baselines/run_baseline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DATA_DIR=../future/data
+DATA_DIR=../data/datasets
 
-DATASET=dummy1K
-RESULTS=../results/$DATASET/random_baseline.csv
+DATASET=datasets_final_tag
+RESULTS=../results/$DATASET/ranking_baseline.csv
 
 DEVICE=cuda
 
-python random_baseline.py \
+python baseline.py \
         --post-tsv $DATA_DIR/$DATASET/post_data.tsv \
         --test-ids $DATA_DIR/$DATASET/test_ids.txt \
         --qa-tsv $DATA_DIR/$DATASET/qa_data.tsv \
