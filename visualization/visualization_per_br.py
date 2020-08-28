24e394bdd628be664796a64e4f0f82bed969a859
imranm3 <imranm3@egr-u-cs-kd-02.rams.adp.vcu.edu>
2020-08-27 21:48:18 -0400

    figure

--- visualization/visualization_per_br.py
+++ visualization/visualization_per_br.py
@@ -38,8 +38,8 @@ def main(args):
         x=alt.X('percentage:Q', title='Percentage',
                 axis=alt.Axis(format='.0f')),
         y=alt.Y('id:N', axis=y_axis,
-                sort=["FQ9", "FQ22", "FQ14", "FQ8", "FQ10",
-                      "FQ12", "FQ19", "FQ18", "FQ17", "FQ2", "FQ6", "FQ3", "FQ5"]),
+                sort=["FQ9", "FQ22", "FQ14",  "FQ8", "FQ19", "FQ10",
+                      "FQ12",  "FQ18", "FQ17", "FQ2", "FQ6", "FQ3", "FQ5"]),
         color=alt.Color(
             'type:N',
             legend=alt.Legend(orient='bottom', title='', padding=-12),
